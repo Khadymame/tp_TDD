6105fd7de4f20196cce6d39350c63bdb080f7390
khadymame <mamekhadywa@gmail.com>
2023-10-02 12:20:32 +0200

    creatioin premier test ko

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -5,4 +5,6 @@ class Test_FizzBuzz (unittest.TestCase):
     def setUp(self):
         self.instance=FizzBuzz()
 
+    def test_affiche_sans_parm(self):
+        self.assertEqual(self.instance.affiche(), "12Fizz4Buzz")
     
